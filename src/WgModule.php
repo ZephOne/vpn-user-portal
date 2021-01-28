e7ab5a1f1bfbad781022af23c28e915e92bd3da2
François Kooman <fkooman@tuxed.net>
2021-01-28 16:53:27 +0100

    implement config generation

--- src/WgModule.php
+++ src/WgModule.php
@@ -43,13 +43,45 @@ class WgModule implements ServiceModuleInterface
              * @return \LC\Common\Http\Response
              */
             function (Request $request, array $hookData) {
-                $httpResponse = $this->httpClient->get('http://localhost:8080/list', [], []);
+                $httpResponse = $this->httpClient->get('http://localhost:8080/info', [], []);
+
+                $wgInfo = Json::decode($httpResponse->getBody());
+
+                // figure out if we have any peers configured, find a free IP
+                // address...
+
+                $minIndex = 1;
+                if (\array_key_exists('Peers', $wgInfo)) {
+                    // we have some peer(s)
+                    foreach ($wgInfo['Peers'] as $peerInfo) {
+                        foreach ($peerInfo['AllowedIPs'] as $allowedIp) {
+                            if (false !== strpos($allowedIp, '.')) {
+                                // IPv4
+                                list(, , , $i) = explode('.', $allowedIp);
+                                if ($i > $minIndex) {
+                                    ++$minIndex;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                $ipFour = '10.10.10.'.($i + 1);
+                $ipSix = 'fd00:1234:1234:1234::'.($i + 1);
+
+                $privateKey = self::generatePrivateKey();
+                $publicKey = self::getPublicKey($privateKey);
 
                 return new HtmlResponse(
                     $this->tpl->render(
                         'vpnPortalWgPeers',
                         [
-                            'peerList' => Json::decode($httpResponse->getBody()),
+                            'pubKey' => $publicKey,
+                            'secKey' => $privateKey,
+                            'ipFour' => $ipFour,
+                            'ipSix' => $ipSix,
+                            'wgHost' => $request->getServerName(),
+                            'wgInfo' => $wgInfo,
                         ]
                     )
                 );
@@ -71,16 +103,37 @@ class WgModule implements ServiceModuleInterface
                 // parse the form fields
                 // XXX make sure content-type is correct
                 $httpResponse = $this->httpClient->postRaw(
-                    'http://localhost:8080/register',
+                    'http://localhost:8080/add_peer',
                     [],
                     $rawPostData
                 );
 
-                var_dump($httpResponse);
-                exit();
-
                 return new RedirectResponse($request->getRootUri().'wg');
             }
         );
+    }
+
+    /**
+     * @return string
+     */
+    private static function generatePrivateKey()
+    {
+        ob_start();
+        passthru('/usr/bin/wg genkey');
+
+        return trim(ob_get_clean());
+    }
+
+    /**
+     * @param string $privateKey
+     *
+     * @return string
+     */
+    private static function getPublicKey($privateKey)
+    {
+        ob_start();
+        passthru("echo $privateKey | /usr/bin/wg genkey");
+
+        return trim(ob_get_clean());
     }
 }
--- views/vpnPortalWgPeers.php
+++ views/vpnPortalWgPeers.php
@@ -1,5 +1,37 @@
 <?php $this->layout('base', ['activeItem' => 'wg', 'pageTitle' => $this->t('WireGuard')]); ?>
 <?php $this->start('content'); ?>
+    <h3>New Peer</h3>
+    <p>Copy &amp; paste this configuration in your WireGuard client and click
+    the <em>Register Me!</em> button to "claim" this configuration.
+    </p>
+
+<pre>
+[Peer]
+PublicKey = <?=$this->e($wgInfo['PublicKey']); ?>
+
+AllowedIPs = 0.0.0.0/32, ::/0
+Endpoint = <?=$this->e($wgHost); ?>:<?=$this->e($wgInfo['ListenPort']); ?>
+
+
+[Interface]
+PrivateKey = <?=$this->e($secKey); ?>
+
+Address = <?=$this->e($ipFour); ?>/24, <?=$this->e($ipSix); ?>/64
+DNS = 9.9.9.9, 2620:fe::fe
+</pre>
+
+<form class="frm" method="post" action="wg_add_peer">
+    <input type="hidden" name="PublicKey" value="<?=$this->e($pubKey); ?>">
+    <input type="hidden" name="IPv4" value="<?=$this->e($ipFour); ?>">
+    <input type="hidden" name="IPv6" value="<?=$this->e($ipSix); ?>">
+    <fieldset>
+        <button type="submit">Register Me!</button>
+    </fieldset>
+</form>
+
+
+
+<?php if (array_key_exists('Peers', $wgInfo)): ?>
     <h3>List of Peers</h3>
     <table class="tbl">
         <thead>
@@ -9,12 +41,12 @@
             </tr>
         </thead>
         <tbody>
-    <?php foreach ($peerList as $peerItem): ?>
+    <?php foreach ($wgInfo['Peers'] as $peerInfo): ?>
         <tr>
-            <td><?= $this->e($peerItem['PublicKey']); ?></td>
+            <td><?= $this->e($peerInfo['PublicKey']); ?></td>
             <td>
                 <ul>
-<?php foreach ($peerItem['AllowedIPs'] as $allowedIp): ?>
+<?php foreach ($peerInfo['AllowedIPs'] as $allowedIp): ?>
                     <li><?= $this->e($allowedIp); ?></li>
 <?php endforeach; ?>
                 </ul>
@@ -23,14 +55,5 @@
     <?php endforeach; ?>
         </tbody>
     </table>
-
-    <h3>Add Peer</h3>
-    <form class="frm" method="post" action="wg_add_peer">
-        <label>Public Key <input type="text" name="PublicKey"></label>
-        <label>IPv4 Address <input type="text" name="IPv4" required></label>
-        <label>IPv6 Address <input type="text" name="IPv6" required></label>
-        <fieldset>
-            <button type="submit">Add</button>
-        </fieldset>
-    </form>
+<?php endif; ?>
 <?php $this->stop('content'); ?>
